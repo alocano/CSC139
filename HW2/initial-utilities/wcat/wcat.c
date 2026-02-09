13bc46d68fed1ad040ed8813d35e223b666b7e5b
alocano <alocano@csus.edu>
2026-02-08 16:39:35 -0800

    Update HW2

---
+++ HW2/initial-utilities/wcat/wcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    // Check if file specified, 
+    if (argc == 1) {
+        return 0;
+    }
+    
+    // Process each file
+    for (int i = 1; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        
+        // Check if file opened successfully
+        if (fp == NULL) {
+            printf("wcat: cannot open file\n");
+            exit(1);
+        }
+        
+        // Read and print file contents line by line
+        char buffer[4096];
+        while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+            printf("%s", buffer);
+        }
+        
+        // Close the file
+        fclose(fp);
+    }
+    
+    return 0;
+}
